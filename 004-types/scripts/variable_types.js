bb5200078e2bbd5936ffa4097ff3892deb31e6c7
Valentine Shemyako <v.shemyako@gmail.com>
2019-02-13 13:40:37 +0300

    Listed different variable types of ES5

---
+++ 004-types/scripts/variable_types.js
@@ -0,0 +1,23 @@
+var number = 10.0;
+var typeNumber = typeof number;
+alert(number + " is of type " + typeNumber);
+
+var word = "hello";
+var typeWord = typeof word;
+alert(word + " is of type " + typeWord);
+
+var predicate = 1 > 2;
+var typeBoolean = typeof predicate;
+alert(predicate + " is of type " + typeBoolean);
+
+var justice = null;
+var typeNull = typeof justice;
+alert(justice + " is of type " + typeNull);
+
+var universeSize;
+var typeUndefined = typeof universeSize;
+alert(universeSize + " is of type " + typeUndefined);
+
+var item = {};
+var typeObject = typeof item;
+alert(item + " is of type " + typeObject);
