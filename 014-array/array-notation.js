27952c04af77fb015d5694e6f6f290da8ef47b6b
Valentine Shemyako <v.shemyako@gmail.com>
2019-02-17 10:38:25 +0300

    Examined array literal notation

---
+++ 014-array/array-notation.js
@@ -0,0 +1,12 @@
+let alphabet = [
+    "a",
+    "b",
+    "c"
+];
+
+console.log(alphabet[0]);
+console.log(alphabet[1]);
+console.log(alphabet[2]);
+console.log(alphabet[120]); // undefined
+
+console.log(alphabet.length); // 3
