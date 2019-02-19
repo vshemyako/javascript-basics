e125ff989aca83dc79f1a7758d9c417ff289516d
Valentine Shemyako <v.shemyako@gmail.com>
2019-02-19 10:21:28 +0300

    Examined differences between let and var variable declarations

---
+++ 023-let-var-differences/let-var-differences.js
@@ -0,0 +1,29 @@
+// 1. let variable cannot be re-declared
+var varName = "First Declaration";
+var varName = "Second Declaration";
+console.log(varName);
+
+let letName = "First Declaration";
+// let letName = "Second Declaration"; // SyntaxError: Identifier 'letName' has already been declared
+console.log(letName);
+
+// 2. let variable is block scoped
+if (true) {
+    var varIf = "Within if block";
+}
+console.log(varIf);
+
+if (true) {
+    let letIf = "Within if block";
+}
+// console.log(letIf); // ReferenceError: letIf is not defined
+
+// 3. let variable declaration is not hoisted
+console.log(varHoisted);
+var varHoisted = "Hoisted";
+
+// console.log(letHoisted); // ReferenceError: letHoisted is not defined
+let letHoisted = "Hoisted";
+
+// TODO: add differences related to closure
+// TODO: add Anki card
