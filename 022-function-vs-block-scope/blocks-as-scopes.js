7a12a13548a04b9b4e288611dc0f88453d693ae5
Valentine Shemyako <v.shemyako@gmail.com>
2019-02-19 09:50:08 +0300

    Examined pre ES6 ways to create non-function scope

--- 022-function-vs-block-scope/blocks-as-scopes.js
+++ 022-function-vs-block-scope/blocks-as-scopes.js
@@ -1,4 +1,4 @@
-// In code prior to ES6? only functions created new scopes
+// In code prior to ES6 only functions, 'with' construct, and try-catch blocks created new scopes
 for (var index = 0; index < 10; index++) {
 }
 console.log(index);
@@ -8,3 +8,20 @@ if (true) {
 }
 console.log(name);
 
+var name = "global-name";
+var object = {
+    name: "within-an-object"
+};
+
+// with creates it own scope from the object passed
+with (object) {
+    console.log(name); // within-an-object is printed
+}
+
+// catch clause also defined its own scope
+try {
+    null();
+} catch (exception) {
+    console.log(exception);
+}
+// console.log(exception); // ReferenceError
