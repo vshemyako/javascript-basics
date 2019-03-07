bedf5cd1bf3045a25f284aa9328f13667aacc3e1
Valentine Shemyako <v.shemyako@gmail.com>
2019-03-07 17:48:54 +0300

    Implemented reverse in place function

--- 049-array-reversion/array-reversion.js
+++ 049-array-reversion/array-reversion.js
@@ -7,3 +7,19 @@ function reverse(array) {
 }
 
 console.log(reverse([1, 2, 3]));
+
+function reverseInPlace(array) {
+    let iterationsNumber = Math.floor(array.length / 2);
+
+    for(let index = 0; index < iterationsNumber; index++) {
+        let left = array[index];
+        let right = array[array.length - index - 1];
+
+        array[index] = right;
+        array[array.length - index - 1] = left;
+    }
+}
+
+let arrayToReverse = [4, 6, 1];
+reverseInPlace(arrayToReverse);
+console.log(arrayToReverse);
